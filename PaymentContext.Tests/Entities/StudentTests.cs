d85db727c76063547553aa03db5caa5d0c078e60
Anderson Jardim <anderson.jardim@onclick.com.br>
2022-09-01 01:08:58 -0300

    Corrupção no Código

---
+++ PaymentContext.Tests/Entities/StudentTests.cs
@@ -0,0 +1,20 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Entities;
+
+namespace PaymentContext.Tests
+{
+    [TestClass]
+    public class StudentTests
+    {
+        [TestMethod]
+        public void AdicionarAssinatura()
+        {
+            var subscription = new Subscription(null);
+            var student = new Student("Anderson", "Jardim", "123456789012", "andersonjardim@gmail.com");
+            //student.Subscriptions.Add(subscription);
+            student.AddSubscription(subscription);
+
+            //var Payment = new PayPalPaymet();
+        }
+    }
+}
