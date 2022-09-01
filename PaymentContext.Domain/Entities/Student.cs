d29f971c8080e5f4dcc7e85e775952a314bdf22e
Anderson Jardim <anderson.jardim@onclick.com.br>
2022-09-01 19:15:59 -0300

    ShouldReturnErrorWhenActiveSubscription

--- PaymentContext.Domain/Entities/Student.cs
+++ PaymentContext.Domain/Entities/Student.cs
@@ -29,6 +29,8 @@ namespace PaymentContext.Domain.Entities
 
         public void AddSubscription(Subscription subscription)
         {
+            //if(subscription.Payments.Payment.Count == 0)
+
             // if(true)
             //     throw new Exception("");
 
@@ -55,6 +57,7 @@ namespace PaymentContext.Domain.Entities
             AddNotifications(new Contract()
                 .Requires()
                 .IsFalse(hasSubscriptionActive, "Student.Subscription", "Você já tem uma assinatura ativa.")
+                .IsGreaterThan(0, subscription.Payments.Count, "Student.Subscription", "Esta assinatura não possui pagamentos.")
             );    
             
             //alternativa 2:
--- PaymentContext.Tests/Entities/StudentTests.cs
+++ PaymentContext.Tests/Entities/StudentTests.cs
@@ -1,7 +1,6 @@
-//using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PaymentContext.Domain.Entities;
-//using PaymentContext.Domain.Enums;
+using PaymentContext.Domain.Enums;
 using PaymentContext.Domain.ValueObjects;
 
 namespace PaymentContext.Tests
@@ -10,20 +9,56 @@ namespace PaymentContext.Tests
     public class StudentTests
     {
         [TestMethod]
-        public void AdicionarAssinatura()
+        public void ShouldReturnErrorWhenActiveSubscription()
         {
-         //   var subscription = new Subscription(null);
-         //   var student = new Student("Anderson", "Jardim", "123456789012", "andersonjardim@gmail.com");
-         //   //student.Subscriptions.Add(subscription);
-         //   student.AddSubscription(subscription);
+            var name = new Name("Bruce", "Wayne");
+            var document = new Document("25319042043", EDocumentType.CPF);
+            var email = new Email("batman@dc.com");
+            var address = new Address("Rua 1", "1234", "Bairro Legal", "Gothan", "SP", "BR", "134000000");
+            //
+            var student = new Student(name, document, email);
+            var subscription = new Subscription(null);
+            var payment = new PayPalPayment("12345678", DateTime.Now, DateTime.Now.AddDays(5), 10, 10, "WAYNE CORP", document, address, email);
 
-            //var Payment = new PayPalPaymet();
+            subscription.AddPayment(payment);
+            student.AddSubscription(subscription);
+            student.AddSubscription(subscription); //De proposito 2x para dar erro
 
-            //var name = new Name("Teste", "Teste");
-            //foreach(var not in name.Notifications){
-            //   not.Message;
-            //}
-
+            Assert.IsTrue(student.Invalid);
         }
+
+        [TestMethod]
+        public void ShouldReturnErrorWhenSubscriptionHasNoPayment()
+        {
+            var name = new Name("Bruce", "Wayne");
+            var document = new Document("25319042043", EDocumentType.CPF);
+            var email = new Email("batman@dc.com");
+            var student = new Student(name, document, email);
+            Assert.Fail();
+        }
+
+        // [TestMethod]
+        // public void ShouldReturnSucessWhenActiveSubscription()
+        // {
+        //     Assert.Fail();
+        // }
+
+
+
+        // [TestMethod]
+        // public void AdicionarAssinatura()
+        // {
+        //  //   var subscription = new Subscription(null);
+        //  //   var student = new Student("Anderson", "Jardim", "123456789012", "andersonjardim@gmail.com");
+        //  //   //student.Subscriptions.Add(subscription);
+        //  //   student.AddSubscription(subscription);
+
+        //     //var Payment = new PayPalPaymet();
+
+        //     //var name = new Name("Teste", "Teste");
+        //     //foreach(var not in name.Notifications){
+        //     //   not.Message;
+        //     //}
+        // }
     }
 }
